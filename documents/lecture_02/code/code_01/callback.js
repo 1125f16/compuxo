1fc36ff7fb5d27c946215af4903cab1aaeffc0e1
abelsan <abel@mit.edu>
2017-09-11 21:38:59 -0400

    AL

---
+++ documents/lecture_02/code/code_01/callback.js
@@ -0,0 +1,12 @@
+var list = [5,8,9,2,7,6,3,1,4];
+
+function callback(list){
+	// --- YourCode ---
+}
+
+function filter(list,callback){
+	// --- YourCode ---
+}
+
+var filtered = filter(list, callback);
+console.log(filtered);
