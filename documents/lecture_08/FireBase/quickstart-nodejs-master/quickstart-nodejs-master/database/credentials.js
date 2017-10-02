95969fdfd333bcb283d83846ca637bf1439781ee
jrw0mitedu <jrwtango@gmail.com>
2017-10-02 13:11:52 -0400

    08

---
+++ documents/lecture_08/FireBase/quickstart-nodejs-master/quickstart-nodejs-master/database/credentials.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/3.9.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAsFliARRevJdcP7sbjYnr7FqWT5bC3CWc",
+    authDomain: "jrwtest1.firebaseapp.com",
+    databaseURL: "https://jrwtest1.firebaseio.com",
+    projectId: "jrwtest1",
+    storageBucket: "jrwtest1.appspot.com",
+    messagingSenderId: "944943294115"
+  };
+  firebase.initializeApp(config);
+</script>
