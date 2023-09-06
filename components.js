edb2e7996de42274052956630c5bc2babdb3793c
digitaltango <jrw@mit.edu>
2023-09-06 10:21:30 -0400

    components update

--- components.js
+++ components.js
@@ -5,49 +5,26 @@ class Submodule extends HTMLElement {
     super();
 
     // attribute content 
-    const title       = this.getAttribute('title');
-    const lecture     = this.getAttribute('lecture');
-    const lectureUrl  = this.getAttribute('lectureUrl');        
-    const video       = this.getAttribute('video');
-    const videoUrl    = this.getAttribute('videoUrl');
-    const camtasia    = this.getAttribute('camtasia');    
-    const camtasiaUrl = this.getAttribute('camtasiaUrl');
-    const exercise    = this.getAttribute('exercise');
-    const exerciseUrl = this.getAttribute('exerciseUrl');
-    const test        = this.getAttribute('test');
-    const testUrl     = this.getAttribute('testUrl');
+    let title  = this.getAttribute('title');
+    let url    = this.getAttribute('url');    
+    let icon   = this.getAttribute('icon');    
 
-    let lectureStr = '';
-    if(lecture){
-      lectureStr = `<a href="${lectureUrl}"><i class="fab fa-slideshare"></i> ${lecture}</a><br />`;
+    if (!title) {
+        title = 'No Title'
+    }
+    if (icon) {
+        icon = `<i class="${icon}"></i>`
+    }
+    else{
+        icon = '<i class="fa-solid fa-file"></i>';
+    }
+    if (!url) {
+        title = 'No-URL';
     }
 
-    let videoStr = '';
-    if(video){
-      videoStr = `<a href="${videoUrl}"><i class="fas fa-video"></i> ${video}</a><br />`;
-    }
-    let camtasiaStr = '';
-    if(camtasia){
-      camtasiaStr = `<a href="${camtasiaUrl}"><i class="fab fa-cuttlefish"></i> ${camtasia}</a><br />`;
-    }
-    let exerciseStr = '';
-    if(exercise){
-      exerciseStr = `<a href="${exerciseUrl}"> <i class="fas fa-tools"></i> ${exercise}</a><br />`;
-    }
-    let testStr = '';
-    if(test){
-      testStr = `<a href="${testUrl}"> <i class="fas fa-vial"></i> ${test}</a><br/>`;
-    }
 
     this.innerHTML = `
-        <h5>${title}</h5>
-        <p>
-          ${lectureStr}
-          ${videoStr}
-          ${camtasiaStr}
-          ${exerciseStr}
-          ${testStr}          
-        </p>
+          <a href="${url}" class="text-decoration-none"> ${icon} ${title}</a><br />
     `;    
 
   }
@@ -64,14 +41,21 @@ class Module extends HTMLElement {
     super();
 
     // attribute content 
-    const title = this.getAttribute('title');
+    let title = this.getAttribute('title');
+    let background = this.getAttribute('background');
+
+    if (!background) {
+        background = 'text-bg-secondary'
+    }
 
     this.innerHTML = `
         <div class="card">
-          <h5 class="card-header">${title}</h5>
-          <div class="card-body">
-            ${this.innerHTML}
-          </div>
+          <h5 class="card-header ${background}">${title}</h5>
+
+            <div class="card-body">
+              ${this.innerHTML}
+            </div>
+
         </div>
     `;    
   }
